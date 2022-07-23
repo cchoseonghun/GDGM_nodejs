d52b365d98f75266fb240c9c2bb5f2ae983832bd
cchoseonghun <cchoseonghun@gmail.com>
2022-07-23 19:47:23 +0900

    basic page of raid

--- server.js
+++ server.js
@@ -45,13 +45,13 @@ app.get('/', (req, res)=>{
 })
 
 app.post('/login', (req, res)=>{
-    let id = req.body.id;
-    db.collection('user').findOne({id: id}, (error, result)=>{
+    let name = req.body.name;
+    db.collection('user').findOne({name: name}, (error, result)=>{
         if(result){
             res.send({code: 1, msg: '기존 아디 로그인', data: result});
         } else {
-            db.collection('user').insertOne({id: id}).then((result)=>{
-                res.send({code: 2, msg: '생성 후 로그인', data: {_id: result.insertedId, id: id}});
+            db.collection('user').insertOne({name: name}).then((result)=>{
+                res.send({code: 2, msg: '생성 후 로그인', data: {_id: result.insertedId, name: name}});
             })
         }
     })
@@ -74,6 +74,31 @@ app.get('/list', (req, res)=>{
     //     console.log('insertId: ' + result.insertedId);
     //     res.send(result);
     // })
+})
+
+app.post('/raid', (req, res)=>{
+    let raid_name = req.body.raid_name;
+    let d_date = req.body.d_date;
+    let d_time = req.body.d_time;
+    let group_id = ObjectId(req.body.group_id);
+    let group_name = req.body.group_name;
+    let members = [{ 
+        _id: ObjectId(req.body.master_id), 
+        name: req.body.master_name, 
+        condition: 0, 
+     }]
+    db.collection('raid').insertOne({raid_name: raid_name, d_date: d_date, d_time: d_time, members: members, group_id: group_id, group_name: group_name}).then((result)=>{
+        res.send({code: 1, msg: '레이드 등록', data: result});
+    })
+})
+
+app.get('/raid', (req, res)=>{
+    let group_id = ObjectId(req.query.group_id);
+    db.collection('raid').find({ group_id: group_id }).toArray().then((result)=>{
+        res.send(result);
+    }).catch((error)=>{
+        console.log('error: ' + error);
+    })
 })
 
 app.get('*', (req, res)=>{
